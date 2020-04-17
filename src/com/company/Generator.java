0e5884799663c273eb5c3a7138d7e059247bfb1e
Yegor Levandovskyi <levandovskyi98@gmail.com>
2020-04-18 00:20:40 +0300

    finishing touches, code refactoring

---
+++ src/com/company/Generator.java
@@ -0,0 +1,89 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.List;
+
+class Generator {
+    private int iterationNumber = 1000000;
+    private int divider = 2147483647;
+    private int factorA = 16807;
+    private int factorB = 48271;
+    private int numbersToCheck = 8;
+
+    private long getGenerator(long number) {
+        return Math.floorMod(number, divider);
+    }
+
+    private long getFactoredGenerator(String factor, long num) {
+        long factoredNumber = getFactoredNumber(factor, num);
+        return getGenerator(factoredNumber);
+    }
+
+    private long getFactoredNumber(String factor, long num) {
+        if (factor.equals("a")) {
+            return num * factorA;
+        }
+
+        if (factor.equals("b")) {
+            return num * factorB;
+        }
+
+        return 1;
+    }
+
+    private boolean lastNEqual(int n, long a, long b) {
+        String binaryA = getBinaryFromGenerator("a", a);
+        String binaryB = getBinaryFromGenerator("b", b);
+        List<String> stringA = Arrays.asList(binaryA.split(""));
+        List<String> stringB = Arrays.asList(binaryB.split(""));
+        return getLastNSublist(n, stringA).equals(getLastNSublist(n, stringB));
+    }
+
+    private List getLastNSublist(int n, List<String> list) {
+        if (n < list.size()) {
+            return list.subList(list.size() - n, list.size());
+        } else {
+            return list;
+        }
+    }
+
+    private String getBinaryFromGenerator(String factor, long num) {
+        long generator = getFactoredGenerator(factor, num);
+        String binaryStringRaw = Long.toBinaryString(generator);
+        return String.format("%32s", binaryStringRaw.replace(" ", "0"));
+    }
+
+    int getMatchedCount(long a, long b) {
+        int matchedTimes = 0;
+
+        for (int i = 0; i <= iterationNumber; i++) {
+            if (lastNEqual(numbersToCheck, a, b)) {
+                matchedTimes++;
+            }
+            a = getFactoredGenerator("a", a);
+            b = getFactoredGenerator("b", b);
+        }
+
+        return matchedTimes;
+    }
+
+    public void setIterationNumber(int iterationNumber) {
+        this.iterationNumber = iterationNumber;
+    }
+
+    public void setDivider(int divider) {
+        this.divider = divider;
+    }
+
+    public void setFactorA(int factorA) {
+        this.factorA = factorA;
+    }
+
+    public void setFactorB(int factorB) {
+        this.factorB = factorB;
+    }
+
+    public void setNumbersToCheck(int numbersToCheck) {
+        this.numbersToCheck = numbersToCheck;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,43 +1,51 @@
 package com.company;
-
-import java.nio.ByteBuffer;
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
-    static int divider = 2147483647;
-    static int factorA = 16807;
-    static int factorB = 48271;
-
+    private static Generator generator = new Generator();
     public static void main(String[] args) {
-	// write your code here
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter first number:");
-        int a = scanner.nextInt();
-        System.out.println("Enter second number:");
-        int b = scanner.nextInt();
-        int multA = getMult("a", a);
-        int multB = getMult("b", b);
-        int generatorA = getGenerator(multA);
-        int generatorB = getGenerator(multB);
-        System.out.println(generatorA);
-        System.out.println(generatorB);
-        System.out.println("First generator:" + Integer.toBinaryString(generatorA) + " second generator:" + Integer.toBinaryString(generatorB));
-    }
-
-    private static int getGenerator(int number) {
-        return number % divider;
-    }
-
-    private static int getMult(String factor, int number) {
-        if (factor.equals("a")) {
-            return number * factorA;
+        System.out.println("Enter two whole numbers to start calculation.");
+        System.out.println("If you wish to end the process, write 'exit'.");
+        while (true) {
+            try {
+                System.out.println("Enter first number:");
+                String stringA = scanner.next();
+                if (checkExit(stringA)) {
+                    printExit();
+                    break;
+                }
+                long a = Long.parseLong(stringA);
+                System.out.println("Enter second number:");
+                String stringB = scanner.next();
+                if (checkExit(stringB)) {
+                    printExit();
+                    break;
+                }
+                long b = Long.parseLong(stringB);
+                System.out.println("Calculating...");
+                int matches = generator.getMatchedCount(a, b);
+                if (matches >= 0) {
+                    System.out.println("Success! Found: " + matches + " matches!");
+                    System.out.println("Finished...");
+                    break;
+                }
+            } catch (NumberFormatException nfe) {
+                System.out.println("Wrong value entered! Please enter whole number!");
+                System.out.println("Starting again...");
+                scanner.nextLine();
+            }
         }
 
-        if (factor.equals("b")) {
-            return number * factorB;
-        }
-
-        return 0;
     }
+
+    private static boolean checkExit(String string) {
+        return string.equals("exit");
+    }
+
+    private static void printExit() {
+        System.out.println("Exiting...");
+    }
+
+
 }
